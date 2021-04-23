fbe0376e01defc2ddfbe4425afb8fe4a41e21504
NSastra <nurhidayatsastra@gmail.com>
2021-04-23 14:27:22 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <hr class="my-3">
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Team</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Steph</td>
+      <td>Curry</td>
+      <td>Warriors</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Klay</td>
+      <td>Thompson</td>
+      <td>Warriors</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Draymond Green</td>
+      <td>Warriors</td>
+    </tr>
+  </tbody>
+</table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//import Slider from '@/components/Slider.vue'
+
+// export default {
+//   name: 'Home',
+//   components: {
+        
+//   }
+// }
+</script>
